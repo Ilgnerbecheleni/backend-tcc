41728663611091d80619cf73892aa89a251d0f3a
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-06-10 13:04:02 -0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const httpsOptions = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
