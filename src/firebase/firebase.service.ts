db0195ea8f090c89c6c8d495490e101ddec65bec
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-06-22 00:17:49 -0300

    service

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -2,26 +2,13 @@
 // src/firebase/firebase.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import * as serviceAccount from '../config/jobs-connect-4ceec-firebase-adminsdk-ihn5l-615642cb22.json';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   onModuleInit() {
     admin.initializeApp({
-      credential: admin.credential.cert({
-        type: process.env.GOOGLE_TYPE,
-        project_id: process.env.GOOGLE_PROJECT_ID,
-        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        auth_uri: process.env.GOOGLE_AUTH_URI,
-        token_uri: process.env.GOOGLE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-      }),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
   }
 
